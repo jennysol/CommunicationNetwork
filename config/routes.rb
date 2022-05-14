f56da30f0d049b1f9dfb90c5f82ff06a867640c4
jennysol <jennyssoliver@gmail.com>
2022-05-13 22:06:49 -0300

    Feature: add new route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
 
   namespace :admin do 
-    root to: "home#index"
+    root to: "admins#index"
     resources :admins
     resources :users, only: :index
     resources :posts, only: [:index, :show]
